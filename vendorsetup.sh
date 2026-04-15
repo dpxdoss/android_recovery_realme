7023960544bc369f55705317f18a4cb5aae6e6ad
Dante <itsdipinbox+github@gmail.com>
2026-04-16 02:42:46 +0530

    Update vendorsetup.sh with licensing and targets
    Added licensing information and updated target devices.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,22 @@
-# ofrp device
-export FOX_TARGET_DEVICES="RMX2027,RMX2020"
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
 FDEVICE="RMX2020"
 
 fox_get_target_device() {
@@ -15,6 +32,10 @@ local chkdev=$(echo "$BASH_SOURCE" | grep \"$FDEVICE\")
 if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
    fox_get_target_device
 fi
+
+# ofrp device
+export FOX_TARGET_DEVICES="RMX2020,RMX2027"
+export TARGET_DEVICE_ALT="RMX2020,RMX2027"
 
 # ofrp settings
 export ALLOW_MISSING_DEPENDENCIES=true
