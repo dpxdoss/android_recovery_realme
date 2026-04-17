6fac1330047108ded509533f354aaa4e4192ea1d
Dante <itsdipinbox+github@gmail.com>
2026-04-18 02:48:49 +0530

    Create patches setup.sh

---
+++ patches/setup.sh
@@ -0,0 +1,6 @@
+# maintainer photo
+# DIR_MAINTAINER=gui/theme/portrait_hdpi/images/Default/About/maintainer.png
+# splash title
+# DIR_SPLASH=gui/theme/portrait_hdpi/splash.xml
+# wipe order
+# DIR_PM=partitionmanager.cpp
