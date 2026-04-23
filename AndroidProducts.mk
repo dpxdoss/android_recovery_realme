717273df6b334264382e02751bfce552ccc1ba01
Dante <266556095+dpxdoss@users.noreply.github.com>
2026-04-23 23:15:44 +0530

    Add fox product makefile and common lunch choices
    Added support for fox_RMX2020.mk and updated lunch choices.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,6 @@
 #
 # Copyright (C) 2020 The Android Open Source Project
 # Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,4 +16,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-     $(LOCAL_DIR)/twrp_RMX2020.mk
+    $(LOCAL_DIR)/twrp_RMX2020.mk \
+    $(LOCAL_DIR)/fox_RMX2020.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_RMX2020-ap2a-userdebug \
+    twrp_RMX2020-ap2a-user \
+    twrp_RMX2020-ap2a-eng \
+    fox_RMX2020-ap2a-userdebug \
+    fox_RMX2020-ap2a-user \
+    fox_RMX2020-ap2a-eng
