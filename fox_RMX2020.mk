e197f3afb590ddd1ce4635b2d6ac079cd70ba331
Dante <266556095+dpxdoss@users.noreply.github.com>
2026-04-23 23:16:16 +0530

    Update fox_RMX2020.mk with device configuration

--- fox_RMX2020.mk
+++ fox_RMX2020.mk
@@ -7,28 +7,39 @@
 #	the Free Software Foundation, either version 3 of the License, or
 #	any later version.
 #
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
 
-# Maintainer Info
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from TWRP config
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/realme/RMX2020/device.mk)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
+
+PRODUCT_DEVICE := RMX2020
+PRODUCT_NAME := fox_RMX2020
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX2020
+PRODUCT_MANUFACTURER := realme
+
+TW_DEVICE_VERSION := OrangeFox for RealmeUI 2.0
+
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# OrangeFox-specific exports
 export OF_MAINTAINER="Dante"
-
-# OTA Patches
 export OF_SUPPORT_OZIP_DECRYPTION=1
 export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
 export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
 export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-
-# Display Info
 export OF_SCREEN_H=2400
 export OF_STATUS_H=80
 export OF_STATUS_INDENT_RIGHT=48
@@ -45,15 +56,9 @@ export OF_DEFAULT_TIMEZONE="GMT-5:30"
 export OF_ENABLE_ALL_PARTITION_TOOLS=1
 export OF_FLASHLIGHT_ENABLE=0
 export OF_ADVANCED_SECURITY=1
-
-# Decryption fix
 export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
 export OF_WIPE_METADATA_AFTER_DATAFORMAT=1
-
-# Partition Info
 export OF_QUICK_BACKUP_LIST="/nvram;/nvdata;/nvcfg;/protect_f;/protect_s;/proinfo;/oppo_custom;/md1img;"
 export OF_DYNAMIC_FULL_SIZE=6685720576
-
-# Logging Patches
 export OF_DISPLAY_FORMAT_FILESYSTEMS_DEBUG_INFO=1
 export OF_DONT_KEEP_LOG_HISTORY=1
